e446e37fd2c4be506be097d5eab7cb6bfe32cb60
Kaychanne <michella.anastasya@itbs.ac.id>
2024-04-18 21:37:28 +0700

    Portofolio

--- view/javascript/landing.js
+++ view/javascript/landing.js
@@ -1,35 +1,3 @@
-// let currentProject;
-// function showDetails(projectId) {
-//     currentProject = projectId;
-//     const modal = document.getElementById('projectModal');
-//     const image = document.getElementById('modalImage');
-//     const title = document.getElementById('projectTitle');
-//     const description = document.getElementById('projectDescription');
-    
-//     image.src = `${projectId}.jpg`;
-//     title.textContent = `Project ${projectId}`;
-//     description.textContent = `Description of Project ${projectId}`;
-//     modal.style.display = 'block';
-// }
-// function closeModal() {
-//     const modal = document.getElementById('projectModal');
-//     modal.style.display = 'none';
-// }
-
-// let likeCount = 0;
-// function like() {
-//     likeCount++;
-//     document.getElementById('likeCount').textContent = `${likeCount} likes`;
-// }
-
-// function comment() {
-//     const commentText = document.getElementById('comment').value;
-//     const commentDiv = document.getElementById('comments');
-//     const newComment = document.createElement('div');
-//     newComment.textContent = commentText;
-//     commentDiv.appendChild(newComment);
-//     document.getElementById('comment').value = '';
-// }
 let currentProject;
     let projectsData = [
         {
