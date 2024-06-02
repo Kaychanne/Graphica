421399710b083be429d581539fe9c68391ac9d91
Kaychanne <michella.anastasya@itbs.ac.id>
2024-06-02 18:37:22 +0700

    css upgrade

--- view/javascript/submit.js
+++ view/javascript/submit.js
@@ -2,20 +2,17 @@ function onPesananSubmit (event){
     event.preventDefault();
 
     const final = Object.fromEntries(new FormData(event.target).entries());
-    // console.log(final);
     $.ajax({
         type: "POST",
         url: "http://localhost:3000/api/1234567890",
         data: JSON.stringify(final),
         contentType: "application/json",
         success: function (data) {
-            // Do something with the response
             console.log("success: " + data.message);
 
             location.reload();
         },
         error: function (xhr, status, error) {
-            // Handle error
             let errorCode = xhr.status;
             let errorText = xhr.statusText;
             let errorMessage = `<span><strong>Error ${errorCode}</strong>: ${errorText}</span>`;
